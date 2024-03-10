1cc484f81dfa56690c996845f22d59a87488d049
matschl12 <matschlgg@gmail.com>
2024-03-10 21:28:06 +0100

    Expandable Movie Cards done Coil done

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.compose.ui.Alignment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.material.icons.Icons
@@ -11,10 +13,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.KeyboardArrowDown
@@ -42,12 +46,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import com.example.movieappmad24.models.Movie
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material.*
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import com.example.movieappmad24.models.getMovies
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
-
+import org.w3c.dom.Text
 
 
 class MainActivity : ComponentActivity() {
@@ -95,20 +110,63 @@ class MainActivity : ComponentActivity() {
     }
 
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun MovieRow(movie: Movie){
-        var showDetails = remember{ mutableStateOf(false) }
+        var showDetails by remember{
+            mutableStateOf(false)
+        }
+
+        val rotationState by animateFloatAsState( //to rotate the arrow icon
+            targetValue = if(showDetails) 180f else 0f
+        )
+
         Column {
-            Card {
-                Box {
-                    Image(painter = painterResource(id = R.drawable.movie_image), contentDescription = "placeholder_image")
+            Card (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .animateContentSize(),
+
+
+            ){
+                Box {//box with the movie banner
+                    AsyncImage(model = movie.images.first(), //takes the first url from the available pictures for each movie
+                        contentDescription = "Movie Banner")
                     Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = "Like" , Modifier.align(Alignment.TopEnd) )
                 }
             }
+            //column with movie title and icon button (arrow) and if arrow is pressed with movie description
             Column(verticalArrangement = Arrangement.SpaceBetween, horizontalAlignment = Alignment.End) {
                 Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-                    Text(text = movie.title)
-                    Icon(imageVector = if (showDetails.value) Icons.Default.KeyboardArrowDown else Icons.Default.KeyboardArrowUp , contentDescription = "Arrows", modifier = Modifier.clickable { showDetails.value = !showDetails.value })
+                    Text(
+                        text = movie.title
+                    )
+                    IconButton(
+                        modifier = Modifier
+                            .clickable {
+                                showDetails = !showDetails
+                            }
+                            .rotate(rotationState),
+
+                        onClick = {
+                            showDetails = !showDetails
+                        }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowDropDown,
+                            contentDescription = "Drop-Down Arrow"
+                        )
+                    }
+                }
+                //when the arrow button is pressed, show the movie description
+                if(showDetails){
+                    Text( //not the most beautiful way, but works for now
+                        text = "Director: " + movie.director
+                                + "\nReleased: " +movie.year
+                                + "\nGenre: " + movie.genre
+                                + "\nActors: " + movie.actors
+                                + "\nRating: " + movie.rating
+                                + "\nPlot: " + movie.plot
+                    )
                 }
             }
         }
@@ -124,7 +182,15 @@ class MainActivity : ComponentActivity() {
     }
 
 
+    @Composable
+    @Preview
+    fun ExpandableCardPreview() //for preview without having to load the emulator
+    {
+       TopAppBar()
+    }
+
 }
+
 
 
 
